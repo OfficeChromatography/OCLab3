cb3a8042eee2d54aa7a716223a536582b40213c3
lucassinggg <lucassinggg@gmail.com>
2020-11-27 12:34:09 +0100

    New PID constants

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -535,9 +535,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 86.58
+  #define DEFAULT_bedKi 11.13
+  #define DEFAULT_bedKd 449.21
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
