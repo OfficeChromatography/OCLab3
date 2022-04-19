536634099d454154b4fb73cfd40226c2aa6d9992
wschwack <wolfgang.schwack@uni-giessen.de>
2022-04-19 18:12:00 +0200

    eagle files added

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -752,7 +752,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 2, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 300, 10000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
